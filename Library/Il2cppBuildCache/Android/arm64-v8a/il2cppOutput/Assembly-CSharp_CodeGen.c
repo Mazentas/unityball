00ecbc3fe6f81e61c91e570629ce6fbf450c54c8
lovebabies <jdnguyen.1995@gmail.com>
2022-07-21 11:20:18 +0700

    init

---
+++ Library/Il2cppBuildCache/Android/arm64-v8a/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,95 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void Ball::Start()
+extern void Ball_Start_m218869060D38F7477CC98EBB540C6015DB639629 (void);
+// 0x00000002 System.Void Ball::Update()
+extern void Ball_Update_mCD5B496FCBE14D32420090CAA070263F5235D3F2 (void);
+// 0x00000003 System.Void Ball::OnMouseDown()
+extern void Ball_OnMouseDown_m484DDD5178CF6BC4C90E1C5F83C7F17685925F00 (void);
+// 0x00000004 System.Void Ball::.ctor()
+extern void Ball__ctor_m76D6811A7C300B95F347A08A5731B731A60E1D25 (void);
+// 0x00000005 System.Void GameManager::Start()
+extern void GameManager_Start_m26461AEF27E44DB8FECCBC19D6C9E228B658BF8E (void);
+// 0x00000006 System.Void GameManager::Update()
+extern void GameManager_Update_mC9303BA7C3117BD861F49F8E36151CC52117E6C1 (void);
+// 0x00000007 System.Void GameManager::ScoreUp()
+extern void GameManager_ScoreUp_mC80F6E0280383DA97BD16EC74E7DCB17DD0A77A8 (void);
+// 0x00000008 System.Void GameManager::Win()
+extern void GameManager_Win_mEE312032712783936AAC9B0EF0CED7BDB345C973 (void);
+// 0x00000009 System.Void GameManager::Restart()
+extern void GameManager_Restart_m1E9741B5443E1FF65B0BE1A20A8F584C90394654 (void);
+// 0x0000000A System.Void GameManager::.ctor()
+extern void GameManager__ctor_mE8666F6D0CA9C31E16B719F79780DC4B0245B64D (void);
+// 0x0000000B System.Void PlayerController::Start()
+extern void PlayerController_Start_m9531F30EC892BDD1758A2EEC724E86EFBDA150A3 (void);
+// 0x0000000C System.Void PlayerController::Update()
+extern void PlayerController_Update_mB31159CAD7DD2329859472554BC9154A83D8E794 (void);
+// 0x0000000D System.Void PlayerController::.ctor()
+extern void PlayerController__ctor_mF30385729DAFDFCB895C4939F6051DCE6C0327FB (void);
+static Il2CppMethodPointer s_methodPointers[13] = 
+{
+	Ball_Start_m218869060D38F7477CC98EBB540C6015DB639629,
+	Ball_Update_mCD5B496FCBE14D32420090CAA070263F5235D3F2,
+	Ball_OnMouseDown_m484DDD5178CF6BC4C90E1C5F83C7F17685925F00,
+	Ball__ctor_m76D6811A7C300B95F347A08A5731B731A60E1D25,
+	GameManager_Start_m26461AEF27E44DB8FECCBC19D6C9E228B658BF8E,
+	GameManager_Update_mC9303BA7C3117BD861F49F8E36151CC52117E6C1,
+	GameManager_ScoreUp_mC80F6E0280383DA97BD16EC74E7DCB17DD0A77A8,
+	GameManager_Win_mEE312032712783936AAC9B0EF0CED7BDB345C973,
+	GameManager_Restart_m1E9741B5443E1FF65B0BE1A20A8F584C90394654,
+	GameManager__ctor_mE8666F6D0CA9C31E16B719F79780DC4B0245B64D,
+	PlayerController_Start_m9531F30EC892BDD1758A2EEC724E86EFBDA150A3,
+	PlayerController_Update_mB31159CAD7DD2329859472554BC9154A83D8E794,
+	PlayerController__ctor_mF30385729DAFDFCB895C4939F6051DCE6C0327FB,
+};
+static const int32_t s_InvokerIndices[13] = 
+{
+	1125,
+	1125,
+	1125,
+	1125,
+	1125,
+	1125,
+	1125,
+	1125,
+	1125,
+	1125,
+	1125,
+	1125,
+	1125,
+};
+extern const CustomAttributesCacheGenerator g_AssemblyU2DCSharp_AttributeGenerators[];
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	13,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	g_AssemblyU2DCSharp_AttributeGenerators,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
