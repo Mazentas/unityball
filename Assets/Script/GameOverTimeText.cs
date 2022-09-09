43c1262fa2f08cdd92913a3653fcc628d77c0e87
duyanh <duyanh.tran8995@gmail.com>
2022-09-09 15:05:07 +0700

    Total time on gameover scene

---
+++ Assets/Script/GameOverTimeText.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameOverTimeText : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Text text = gameObject.GetComponent<Text>();
+        text.text = Timer.GetTotalTime();
+    }
+
+}
